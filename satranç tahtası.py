0300e51440c1e77f9ee6a69f8272c09176dbbbe4
MelihCoban08 <150540666+MelihCoban08@users.noreply.github.com>
2023-11-23 21:13:20 +0300

    Add files via upload

---
+++ satranç tahtası.py
@@ -0,0 +1,8 @@
+kare = ["B","W","B","W","B","W","B","W",]
+karee = ["W","B","W","B","W","B","W","B",]
+for range in [9]:
+    for i in kare:
+        for i in karee:
+            print(kare)
+            print(karee)
+
