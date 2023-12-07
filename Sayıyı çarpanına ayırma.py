ad64e1554484c696bcec5d0cdf8a144223b1d07b
MelihCoban08 <150540666+MelihCoban08@users.noreply.github.com>
2023-12-07 23:11:04 +0300

    Add files via upload

---
+++ Sayıyı çarpanına ayırma.py
@@ -0,0 +1,15 @@
+a = input()
+i = 0
+listiki = []
+listüç = []
+listbes = []
+listyedi = []
+while i % 1 == 0:
+    if i%2 == 0:
+        listiki.append(i)
+    if i%3 == 0:
+        listüç.append(i)
+    if i%5 == 0:
+        listbes.append(i)
+    if i%7 == 0:
+        listyedi.append(i)
