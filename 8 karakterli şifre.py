122f81425c80273ef60d77cd748d52ed4cc94b89
MelihCoban08 <150540666+MelihCoban08@users.noreply.github.com>
2023-12-07 22:49:52 +0300

    Add files via upload

---
+++ 8 karakterli şifre.py
@@ -0,0 +1,7 @@
+i = 9
+sifre = input("bir şifre girin:")
+karakter_sayisi = len(sifre)
+while karakter_sayisi < i:
+    print("Şifre kaydedildi")
+    break
+else:print("8 karakterli şifre giriniz")
