d591d49eb5b8b738b8b177ba50e02e3b046179db
MelihCoban08 <150540666+MelihCoban08@users.noreply.github.com>
2023-12-09 16:36:06 +0300

    Update 8 karakterli şifre.py

--- 8 karakterli şifre.py
+++ 8 karakterli şifre.py
@@ -1,7 +1,7 @@
-i = 9
+i = 8
 sifre = input("bir şifre girin:")
 karakter_sayisi = len(sifre)
-while karakter_sayisi < i:
+while karakter_sayisi == i:
     print("Şifre kaydedildi")
     break
 else:print("8 karakterli şifre giriniz")
