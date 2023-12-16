c8826ada0c0f0875e22fa701c121aa42fcb53b1f
MelihCoban08 <150540666+MelihCoban08@users.noreply.github.com>
2023-12-16 19:22:35 +0300

    Add files via upload

---
+++ 12. hafta.py
@@ -0,0 +1,49 @@
+def topla(a,b,c):
+    print("bu sayıların toplamı",a+b+c)
+
+a = int(input("Sayı gir"))
+b = int(input("Sayı gir"))
+c = int(input("Sayı gir"))
+topla(a,b,c)
+
+def cikar(q,w,e):
+    print("bu sayıların farkı",q-w-e)
+
+q = int(input("Sayı gir"))
+w = int(input("Sayı gir"))
+e = int(input("Sayı gir"))
+cikar(q,w,e)
+
+
+def carp(r,t,y):
+    print("bu sayıların çarpımı",t*r*y)
+
+r = int(input("Sayı gir"))
+t = int(input("Sayı gir"))
+y = int(input("Sayı gir"))
+carp(r,t,y)
+
+
+def usal(u,i,o):
+    print("bu sayıların üssü",o**i**u)
+
+u = int(input("Sayı gir"))
+i = int(input("Sayı gir"))
+o = int(input("Sayı gir"))
+usal(u,i,o)
+
+def bol(p,m,n):
+    print("bu sayıların sırasıyla bölümü",p/m/n)
+p = int(input("Sayı gir"))
+m = int(input("Sayı gir"))
+n = int(input("Sayı gir"))
+bol(p,m,n)
+
+def modal(s,d,v):
+    print("bu sayılarının modu",s%d%v)
+
+s = int(input("Sayı gir"))
+d = int(input("Sayı gir"))
+v = int(input("Sayı gir"))
+modal(s,d,v)
+
