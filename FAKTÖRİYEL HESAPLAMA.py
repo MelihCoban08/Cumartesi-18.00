55c7705bef901f3beb221e8fec7eb512a8ea3beb
MelihCoban08 <150540666+MelihCoban08@users.noreply.github.com>
2024-01-10 23:30:24 +0300

    Add files via upload

---
+++ FAKTÖRİYEL HESAPLAMA.py
@@ -0,0 +1,12 @@
+n = int(input("Lütfen faktöriyelini bulmak istediğiniz sayıyı giriniz\n"))
+faktoriyel = 1
+i = 1
+
+if n >= 0:
+    while i <= n:
+        faktoriyel *= i
+        i += 1
+print(n,"sayısının faktoriyeli",faktoriyel,"budur")
+
+if n < 0:
+    print("Girdiğiniz sayı 0 dan büyük olmalı")
