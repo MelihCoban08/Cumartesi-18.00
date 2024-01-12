2f555983c6548d0efaae282314703aeb8496b4f1
MelihCoban08 <150540666+MelihCoban08@users.noreply.github.com>
2024-01-13 00:00:36 +0300

    Add files via upload

---
+++ Kelime tersten yazdırma.py
@@ -0,0 +1,11 @@
+def tersyaz ():
+    while True:
+        x = str(input("Kelime giriniz"))
+        for i in range(len(x) -1, -1, -1):
+            print(x[i], end="")
+
+tersyaz()
+
+
+
+"internetden baktım"
